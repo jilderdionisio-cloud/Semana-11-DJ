3e1da725b2ed17c3f1894f1fa0ad2e3274c9353d
Adriana Chinchayhuara <adriana.chinchayhuar@tecsup.edu.pe>
2026-05-31 16:30:40 -0500

    Configurar alias de imports del proyecto

--- Adriana/CineSpoilerS/vite.config.ts
+++ Adriana/CineSpoilerS/vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import path from "path";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
