9f318ea7ecab7203576fa4eb50990ed3f4c2d60a
Jilder Dionisio <jilder.dionisio@tecsup.edu.pe>
2026-05-30 21:19:38 -0500

    Add axios and configure VITE_TMDB_API_KEY (env example); tmdb helper using axios

--- Jilder_Dionisio/src/lib/tmdb.ts
+++ Jilder_Dionisio/src/lib/tmdb.ts
@@ -1,10 +1,23 @@
+import axios from 'axios'
+
 export const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 export const TMDB_WEB_URL = 'https://www.themoviedb.org/'
 
-export function buildTmdbUrl(path: string, apiKey = 'YOUR_TMDB_API_KEY') {
-  return `${TMDB_BASE_URL}${path}?api_key=${apiKey}`
+const API_KEY = (import.meta.env.VITE_TMDB_API_KEY as string) || ''
+
+export function buildTmdbUrl(path: string, query = '') {
+  const sep = path.includes('?') ? '&' : '?'
+  const q = query ? `&${query}` : ''
+  return `${TMDB_BASE_URL}${path}${sep}api_key=${API_KEY}${q}`
 }
 
-export function posterUrl(path: string) {
+export function posterUrl(path: string | undefined | null) {
   return path ? `https://image.tmdb.org/t/p/w500${path}` : ''
+}
+
+export async function fetchFromTmdb(path: string, query = '') {
+  if (!API_KEY) throw new Error('VITE_TMDB_API_KEY is not defined in environment')
+  const url = buildTmdbUrl(path, query)
+  const res = await axios.get(url)
+  return res.data
 }
